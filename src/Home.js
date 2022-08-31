769356d2a7115e182bd938bb91f77b0afa61cb1f
DevMenna <dev.mennaalaa@gmail.com>
2022-08-31 16:51:39 +0200

    updating the site's state

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,17 +8,23 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+const [name,setName] = useState('mario')
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs);
     }
 
-
+    useEffect(() => {
+        console.log("I'm running")
+        
+    } , [name])
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title= "All blogs" handleDelete={handleDelete}></BlogList>
-
+            <button onClick={() => setName('luigi')}>Change name</button>
+            <p>{ name }</p>
         </div>
      );
 }
